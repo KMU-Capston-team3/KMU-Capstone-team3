f618890689dbce4a07c0ff3f7a4cbafb05a22941
junmin-Chang <chjm219@kookmin.ac.kr>
2024-06-18 19:20:12 +0900

    route handler naming 변경 및 빈 주차자리 util 함수로 분리

--- routes/empty_space.py
+++ routes/empty_space.py
@@ -1,36 +1,13 @@
 from flask import Blueprint, current_app
-from utils.capture import capture_and_save
-import os
-import pytesseract
-import cv2
+from utils.get_empty_space import get_empty_space
 
 empty_space_bp = Blueprint('empty_space', __name__)
 
+
 # 빈자리 요청 route
 @empty_space_bp.route("/", methods=["POST"])
-def get_empty_space():
-   
-    filename = capture_and_save() 
-    # 저장된 image 를 바탕으로 이미지 처리
-    image_path = os.path.join(current_app.config['IMAGE_FOLDER'], filename)
-
-    img_color = cv2.imread(image_path)
-    img_blur = cv2.GaussianBlur(img_color, (15, 15), 0)
-    height, width = img_blur.shape[:2]
-        
-    img_hsv = cv2.cvtColor(img_blur, cv2.COLOR_BGR2HSV)
-    lower_blue = (115-10, 30, 30)
-    upper_blue = (130+10, 255, 255)
-
-    img_mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
-    img_result = cv2.bitwise_and(img_blur, img_blur, mask = img_mask)
- 
-    # cv2.imshow('img_mask', img_mask) masking 된 이미지 표시
-    # cv2.imshow('img_color', img_result) bitwise 연산으로 이진화된 이미지 표시
-	  
-    # 이미지 처리 후 이미지에서 텍스트 추출함
-    empty_space = pytesseract.image_to_string(img_result, config='--psm 6')
-
+def get_empty_space_handler():
+    empty_space = get_empty_space()
     return {
         "version": "2.0",
         "template": {
@@ -43,3 +20,11 @@ def get_empty_space():
         ]
     }
             }
+
+def get_empty_space_number():
+    print("실행")
+    empty_space = get_empty_space()
+    print(len(empty_space.split(" ")))
+    
+    return empty_space.split(" ")
+                    
--- routes/stream.py
+++ routes/stream.py
@@ -24,7 +24,7 @@ def gen_frames():
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @stream_bp.route('/')
-def get_stream():
+def get_stream_handler():
     # 만들어진 프레임으로 응답을 만듬
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
