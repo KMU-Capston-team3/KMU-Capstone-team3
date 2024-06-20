93e7acfee367e5174231e98dbe5f70df839997fb
junmin-Chang <chjm219@kookmin.ac.kr>
2024-06-20 15:50:52 +0900

    Remove cached files based on .gitignore rules

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SCHEDULER_API_ENABLED = True
-    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    IMAGE_FOLDER = os.path.join(ROOT_DIR, 'images')
-
--- db/mongo.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from datetime import datetime, timedelta
-import random
-
-# mongodb 
-client = MongoClient('localhost', 27017)
-db = client.flask_db
-collection = db.empty_space_numbers
