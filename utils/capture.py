89cbbdc063733d61c921a9733b6d3cff950691aa
junmin-Chang <chjm219@kookmin.ac.kr>
2024-07-04 13:43:59 +0900

    s3 연결 및 이미지 업로드,반환  로직 변경

--- utils/capture.py
+++ utils/capture.py
@@ -2,14 +2,28 @@ from utils.camera_manager import lock, camera
 from datetime import datetime
 from flask import current_app
 import os
+import boto3
+from dotenv import load_dotenv
+
+load_dotenv()
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    region_name='ap-northeast-2'
+)
 
 def capture_and_save():
     with lock:
         timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-        image_path = os.path.join(current_app.config['IMAGE_FOLDER'], f'image_{timestamp}.jpg') 
-        
+        filename = f'image_{timestamp}.jpg'
         # 이미지 파일 위에 정의한 파일 경로에 저장
+        image_path = os.path.join(current_app.config['IMAGE_FOLDER'], f'image_{timestamp}.jpg') 
         camera.capture_file(image_path)
-    filename = f'image_{timestamp}.jpg'
- 
-    return filename
+
+    
+    s3.upload_file(image_path, os.environ.get("BUCKET_NAME"), filename)
+    image_url = f"https://{os.environ.get("BUCKET_NAME")}.s3.ap-northeast-2.amazonaws.com/{filename}"
+    os.remove(image_path)
+    return image_url
