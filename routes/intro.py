7f46e12f1f4063904c46488970c10f1aadbb8e95
juyoungggg <eun9455@gmail.com>
2024-07-10 22:34:42 +0900

    intro 버튼 별로 설명 분리

--- routes/intro.py
+++ routes/intro.py
@@ -9,24 +9,27 @@ def intro():
     "template": {
     "outputs": [
         {
-        "textCard": {
-            "title": "반갑습니다! 여러분의 빠른 주차를 도와드릴 주차맨이에요!",
-            "description": "제가 가진 기능들에 대해 알려드릴게요!",
+        "basicCard": {
+            "title": "기능 시연은 채팅 창 위의 흰색 칸으로 된 메뉴로 진행해주세요!",
+            "description": "반갑습니다! 여러분의 빠른 주차를 도와드릴 주차맨이에요!\n제가 가진 기능들에 대해 알려드릴게요.",
+            "thumbnail": {
+                "imageUrl": "https://park-awss3-bucket.s3.ap-northeast-2.amazonaws.com/parkingman.jpg"
+            },
             "buttons": [
             {
                 "action": "message",
                 "label": "시간대 별 빈자리 수 예측",
-                "messageText": "저는 30분마다 주차장 사진을 촬영하고, 남아있는 빈 자리 수를 DB에 저장해요. 날짜, 시간, 요일, 공휴일 여부에 따라 오늘 0시~23시에 주차 공간이 얼마나 남아있을 지 예측해서 알려드릴게요."
+                "messageText": "시간대 별 빈자리 수 예측"
             },
             {
                 "action": "message",
                 "label": "주차장 사진 촬영",
-                "messageText": "요청하시는 즉시 주차장 전체 사진을 찍어서 보내드려요."
+                "messageText": "주차장 사진 촬영"
             },
             {
                 "action": "message",
                 "label": "빈 자리 찾기",
-                "messageText": "주차장 사진을 찍고, 주차장 바닥의 자리 번호를 인식해 빈 공간의 번호들을 알려드릴게요."
+                "messageText": "빈 자리 찾기"
             }
             ]
         }
@@ -34,3 +37,49 @@ def intro():
     ]
     }
     }
+
+@intro_bp.route("/empty_space/predict", methods=["POST"])
+def intro_empty_space_predict():
+    return {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": "날짜, 시간, 주말/공휴일 여부에 따라 오늘 0시~23시에 주차 공간이 얼마나 남아있을 지 예측해서 알려드릴게요."
+                }
+            }
+        ]
+    }
+}
+
+
+@intro_bp.route("/capture", methods=["POST"])
+def intro_capture():
+    return {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": "요청하시는 즉시 주차장 전체 사진을 찍어서 보내드려요. 여러 사람이 동시에 요청 시 요청하신 순서에 따라 시간이 5~10초 정도 소요될 수 있어요."
+                }
+            }
+        ]
+    }
+}
+
+@intro_bp.route("/empty_space", methods=["POST"])
+def intro_empty_space():
+    return {
+    "version": "2.0",
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": "주차장 사진을 촬영 후 글자를 인식한 뒤 빈 공간의 번호들을 알려드릴게요."
+                }
+            }
+        ]
+    }
+}
